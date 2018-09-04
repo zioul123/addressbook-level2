9c00285fb163e7a64f8c7d652f193a9004fff75e
zioul123 <zioul.rsmod123@gmail.com>
2018-09-04 11:37:18 +0800

    UtisTest.java: Add JUnit tests for isAnyNull method.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -36,6 +36,36 @@ public class UtilsTest {
         assertNotUnique(null, "a", "b", null);
     }
 
+    @Test
+    public void isAnyNull() throws Exception {
+        Object x = null;
+        
+        // no arguments
+        assertFalse(Utils.isAnyNull());
+        
+        // one argument
+        assertFalse(Utils.isAnyNull("a"));
+        assertFalse(Utils.isAnyNull(1));
+        assertFalse(Utils.isAnyNull(new Object()));
+        assertTrue(Utils.isAnyNull(x));
+
+        // two arguments
+        assertFalse(Utils.isAnyNull("abs", "abd"));
+        assertFalse(Utils.isAnyNull(14, 553));
+        assertFalse(Utils.isAnyNull(3000, "asdf"));
+        assertTrue(Utils.isAnyNull(null, 1555));
+        assertTrue(Utils.isAnyNull("lflflf", null));
+        assertTrue(Utils.isAnyNull(new int[5], null));
+
+        // three arguments
+        assertFalse(Utils.isAnyNull("btbtbt", "lflfl", 5555));
+        assertFalse(Utils.isAnyNull(14, 553, "abcd"));
+        assertFalse(Utils.isAnyNull(3000, "asdf", new float[4]));
+        assertTrue(Utils.isAnyNull(null, 1555, null));
+        assertTrue(Utils.isAnyNull("lflflf", 15353, null));
+        assertTrue(Utils.isAnyNull(new int[5], null, "abcde"));
+    }
+    
     private void assertAreUnique(Object... objects) {
         assertTrue(Utils.elementsAreUnique(Arrays.asList(objects)));
     }
