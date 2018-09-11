62fa9aed1418faa716034c09a9d1792bff4d8c11
zioul123 <zioul.rsmod123@gmail.com>
2018-09-11 10:35:39 +0800

    Add the stats command but not implemented yet.

---
+++ src/seedu/addressbook/commands/StatsCommand.java
@@ -0,0 +1,25 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class StatsCommand extends Command {
+
+    public static final String COMMAND_WORD = "stats";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Shows statistics of the address book.\n"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+}
